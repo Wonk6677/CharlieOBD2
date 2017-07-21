470ee83667682e275f236079c4961de1abb54853
Wonk6677 <bbament@gmail.com>
2017-07-21 22:55:51 +0100

    1.419

--- Charlie.py
+++ Charlie.py
@@ -6,16 +6,16 @@ from kivy.lang import Builder
 Builder.load_file('screenmanager.kv')
 
 
-class MainScreen(Screen):
-    hue = NumericProperty(122)
+#class MainScreen(Screen):
+ #   hue = NumericProperty(122)
 
 
 class ScreenManagerApp(App):
 
     def build(self):
         root = ScreenManager()
-        for x in range(4):
-            root.add_widget(MainScreen(name='Screen %d' % x))
+       # for x in range(4):
+            root.add_widget(MainScreen(name='Screen1'))
         return root
 
 if __name__ == '__main__':
