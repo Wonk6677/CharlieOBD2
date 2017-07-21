0d6b2c46dd01ded5efe1aedb338e447e1a98a5e6
Wonk6677 <bbament@gmail.com>
2017-07-21 20:57:38 +0100

    1.4

--- Charlie.py
+++ Charlie.py
@@ -7,7 +7,7 @@ Builder.load_file('screenmanager.kv')
 
 
 class CustomScreen(Screen):
-    hue = NumericProperty(0)
+ #   hue = NumericProperty(0)
 
 
 class ScreenManagerApp(App):
@@ -17,25 +17,25 @@ class ScreenManagerApp(App):
         for x in range(4):
             root.add_widget(CustomScreen(name='Screen %d' % x))
         return root
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import NumericProperty
-from kivy.lang import Builder
+#from kivy.app import App
+#from kivy.uix.screenmanager import ScreenManager, Screen
+#from kivy.properties import NumericProperty
+#from kivy.lang import Builder
 
-Builder.load_file('screenmanager.kv')
-
-
-class CustomScreen(Screen):
-    hue = NumericProperty(0)
+#Builder.load_file('screenmanager.kv')
 
 
-class ScreenManagerApp(App):
+#class CustomScreen(Screen):
+  #  hue = NumericProperty(0)
 
-    def build(self):
-        root = ScreenManager()
-        for x in range(4):
-            root.add_widget(CustomScreen(name='Screen %d' % x))
-        return root
+
+#class ScreenManagerApp(App):
+
+ #   def build(self):
+  #      root = ScreenManager()
+   #     for x in range(4):
+    #        root.add_widget(CustomScreen(name='Screen %d' % x))
+     #   return root
 
 if __name__ == '__main__':
     ScreenManagerApp().run()
